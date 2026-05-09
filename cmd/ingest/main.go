4b66e0bd655c2439223ed49ebaf1417b11be0c06
Brooks YUAN <hktkzyx@yeah.net>
2026-05-10 06:39:35 +0800

    chore(release): 准备 v0.1.1

--- cmd/ingest/main.go
+++ cmd/ingest/main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/hktkzyx/ingest/internal/template"
 )
 
-var version = "0.1.0"
+var version = "0.1.1"
 
 const defaultTemplate = "{date_start}[_{date_end}]-{event_name}/origin-{device_name}"
 
